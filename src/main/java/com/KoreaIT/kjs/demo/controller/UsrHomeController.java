8ce9e73da89525f3483e6d60b6cb91cdfc835492
Joonseon-Kwak12 <kjoonseon12@gmail.com>
2023-04-13 16:40:31 +0900

    액션 메서드에서 각각의 리턴타입 실험

--- src/main/java/com/KoreaIT/kjs/demo/controller/UsrHomeController.java
+++ src/main/java/com/KoreaIT/kjs/demo/controller/UsrHomeController.java
@@ -1,8 +1,17 @@
 package com.KoreaIT.kjs.demo.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Controller
 public class UsrHomeController {
@@ -24,4 +33,62 @@ public class UsrHomeController {
 		this.count = count;
 		return "count의 값이 " + this.count + 	"으로 설정되었습니다.";
 	}
+	
+	@RequestMapping("/usr/home/getInt")
+	@ResponseBody
+	public int getInt() {
+		return 12454315;
+	}
+	
+	@RequestMapping("/usr/home/getChar")
+	@ResponseBody
+	public char getChar() {
+		return 65;
+	}
+	
+	@RequestMapping("/usr/home/getString")
+	@ResponseBody
+	public String getString() {
+		return "아무 String";
+	}
+	
+	@RequestMapping("/usr/home/getDouble")
+	@ResponseBody
+	public Double getDouble() {
+		return 4.11456124651;
+	}
+	
+	@RequestMapping("/usr/home/getMap")
+	@ResponseBody
+	public Map<String, Object> getMap() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("철수 나이", 22);
+		map.put("영희 나이", 33);
+		return map;
+	}
+	
+	@RequestMapping("/usr/home/getArticle")
+	@ResponseBody
+	public Article getArticle() {
+		Article article = new Article();
+		return article;
+	}
+	
+	@RequestMapping("/usr/home/getList")
+	@ResponseBody
+	public List<Article> getList() {
+		List<Article> articleList = new ArrayList<>();
+		articleList.add(new Article(1, "제목1", "내용1"));
+		articleList.add(new Article(2, "제목2", "내용2"));
+		return articleList;
+	}
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Article {
+	private int id;
+	private String title;
+	private String body;
 }

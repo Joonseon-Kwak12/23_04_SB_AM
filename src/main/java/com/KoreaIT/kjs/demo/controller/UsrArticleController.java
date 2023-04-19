83bc8cb2047b26fec9a5c9c8b50feebcc1f4eff0
Joonseon-Kwak12 <kjoonseon12@gmail.com>
2023-04-19 13:33:18 +0900

    doMydify, do Delete 로그인 체크, 권한 체크 수정

--- src/main/java/com/KoreaIT/kjs/demo/controller/UsrArticleController.java
+++ src/main/java/com/KoreaIT/kjs/demo/controller/UsrArticleController.java
@@ -35,16 +35,16 @@ public class UsrArticleController {
 		Integer memberId = (Integer) httpSession.getAttribute("loginedMemberId");
 
 		if (memberId == null) {
-			return ResultData.from("F-1", "로그인 후 이용해주세요.");
+			return ResultData.from("F-A", "로그인 후 이용해주세요.");
 		}
 
 		Article article = articleService.getArticle(id);
 		if (article == null) {
-			return ResultData.from("F-2", Ut.f("%d번 글은 존재하지 않습니다.", id), id);
+			return ResultData.from("F-1", Ut.f("%d번 글은 존재하지 않습니다.", id), id);
 		}
 		
 		if (memberId != article.getMemberId()) {
-			return ResultData.from("F-3", "수정 권한이 없습니다.");
+			return ResultData.from("F-2", Ut.f("%d번 글에 대한 권한이 없습니다.", id));
 		}
 		
 		articleService.modifyArticle(id, title, body);
@@ -59,16 +59,16 @@ public class UsrArticleController {
 		Integer memberId = (Integer) httpSession.getAttribute("loginedMemberId");
 
 		if (memberId == null) {
-			return ResultData.from("F-1", "로그인 후 이용해주세요.");
+			return ResultData.from("F-A", "로그인 후 이용해주세요.");
 		}
 		
 		Article article = articleService.getArticle(id);
 		if (article == null) {
-			return ResultData.from("F-2", Ut.f("%d번 글은 존재하지 않습니다.", id), id);
+			return ResultData.from("F-1", Ut.f("%d번 글은 존재하지 않습니다.", id), id);
 		}
 		
 		if (memberId != article.getMemberId()) {
-			return ResultData.from("F-3", "삭제 권한이 없습니다.");
+			return ResultData.from("F-2", Ut.f("%d번 글에 대한 권한이 없습니다.", id));
 		}
 		
 		articleService.deleteArticle(id);
@@ -83,14 +83,14 @@ public class UsrArticleController {
 		Integer loginedMemberId = (Integer) httpSession.getAttribute("loginedMemberId");
 		
 		if (loginedMemberId == null) {
-			return ResultData.from("F-1", "로그인 후 이용해주세요.");
+			return ResultData.from("F-A", "로그인 후 이용해주세요.");
 		}
 		
 		if (Ut.empty(title)) {
-			return ResultData.from("F-2", "제목을 입력해주세요.");
+			return ResultData.from("F-1", "제목을 입력해주세요.");
 		}
 		if (Ut.empty(title)) {
-			return ResultData.from("F-3", "내용을 입력해주세요.");
+			return ResultData.from("F-2", "내용을 입력해주세요.");
 		}
 		
 		ResultData<Integer> writeArticleRd = articleService.writeArticle(loginedMemberId, title, body);

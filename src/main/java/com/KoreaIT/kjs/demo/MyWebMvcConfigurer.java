b5f0fd0c8c91aa960d1e01f3b60b28163b5b3641
Joonseon-Kwak12 <kjoonseon12@gmail.com>
2023-04-21 14:26:22 +0900

    NeedLoginInterceptor 구현 및 MyWebMvcConfigurer 등록

--- src/main/java/com/KoreaIT/kjs/demo/MyWebMvcConfigurer.java
+++ src/main/java/com/KoreaIT/kjs/demo/MyWebMvcConfigurer.java
@@ -6,18 +6,26 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.KoreaIT.kjs.demo.interceptor.BeforeActionInterceptor;
+import com.KoreaIT.kjs.demo.interceptor.NeedLoginInterceptor;
 
 @Configuration
 public class MyWebMvcConfigurer implements WebMvcConfigurer {
 	// BeforeActionInterceptor 불러오기
 	@Autowired
 	BeforeActionInterceptor beforeActionInterceptor;
-
+	// NeedLoginInterceptor 불러오기
+	@Autowired
+	NeedLoginInterceptor needLoginInterceptor;
+	
 	// 인터셉터 적용
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**")
 				.excludePathPatterns("/error");
 		// 모든 것을 인터셉터에서 먼저 처리하는데 resource랑 error만 제외
+		registry.addInterceptor(needLoginInterceptor)
+			.addPathPatterns("/usr/article/write").addPathPatterns("/usr/article/doWrite")
+			.addPathPatterns("/usr/article/modify").addPathPatterns("/usr/article/doModify")
+			.addPathPatterns("/usr/article/delete").addPathPatterns("/usr/article/doDelete");
 	}
 
 }
